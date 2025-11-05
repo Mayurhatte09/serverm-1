917d90ef5d94917ae7007c1cfa3c301432cc1a83
Mayur Hatte <143870150+Mayurhatte09@users.noreply.github.com>
2025-11-05 23:23:42 +0530

    Create user model with schema definitions

---
+++ src/models /user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    department: {
+        type:String,
+        //required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
